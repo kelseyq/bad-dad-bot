bff00f10cab29f676149c05c3ed850355397a7cf
Kelsey Gilmore-Innis <kelsey@sexualhealthinnovations.org>
2016-12-25 21:23:53 -0800

    more hackery

--- tweet.py
+++ tweet.py
@@ -16,10 +16,13 @@ terms = ['tech worker', 'software engineer', 'computer programmer', 'software de
 
 nope_words = [
     "african",
+    "asian",
     "bitch",
     "black",
+    "blk",
     "chinaman",
     "chinamen",
+    "chinese",
     "chink",
     "crip",
     "cunt",
@@ -32,6 +35,7 @@ nope_words = [
     "jap",
     "kike",
     "lunatic",
+    "latin",
     "negro",
     "nigga",
     "nigger",
@@ -55,14 +59,13 @@ nope_words = [
     "estate",
     "game",
     "developer update",
+    'radio',
 ]
-
 
 def filter_tweets(results, search_term, seen_tweets):
     return [seen_tweets.add(result['retweeted_status']['text']) or result for result in results['statuses'] if
                   result.get('retweeted_status') and
                   result['retweeted_status']['text'] not in seen_tweets and
-                  len(result['entities']['urls']) == 0 and
                   len(result['retweeted_status']['entities']['urls']) == 0 and
                   len(result['retweeted_status']['entities']['user_mentions']) == 0 and
                   result['retweeted_status']['favorite_count'] > 1 and
@@ -71,18 +74,24 @@ def filter_tweets(results, search_term, seen_tweets):
 
 
 def get_tweets_for_term(search_term, seen_tweets):
-    first_page = twitter.search(q=search_term, lang='en', result_type='recent', count=100)
+    first_page = twitter.search(q=search_term, lang='en', result_type='mixed', count=100)
     if first_page.get('statuses'):
+        #search_term = urllib.quote_plus('"' + search_term + '"')
         #result['retweeted_status']['text']
         #json.dumps(result, indent=4)
         tweets = filter_tweets(first_page, search_term, seen_tweets)
-        # max_id = first_page['statuses'][-1]['id_str']
-        # second_page = twitter.search(q=search_term, lang='en', result_type='recent', max_id=max_id)
-        # tweets = filter_tweets(second_page, search_term, seen_tweets)
-        # max_id = second_page['statuses'][-1]['id_str']
-        # third_page = twitter.search(q=search_term, lang='en', result_type='recent', max_id=max_id)
-        # tweets = tweets + filter_tweets(third_page, search_term, seen_tweets)
+        orig_length = len(tweets)
+        if len(first_page['statuses']) > 0:
+            max_id = first_page['statuses'][-1]['id_str']
+            second_page = twitter.search(q=search_term, lang='en', result_type='recent', max_id=max_id)
+            tweets = tweets + filter_tweets(second_page, search_term, seen_tweets)
+            if len(second_page['statuses']) > 0:
+                max_id = second_page['statuses'][-1]['id_str']
+                third_page = twitter.search(q=search_term, lang='en', result_type='recent', max_id=max_id)
+                tweets = tweets + filter_tweets(third_page, search_term, seen_tweets)
         print(str(len(tweets)) + " results for search term " + search_term)
+        if len(tweets) > orig_length:
+            print("pagination added " + str(len(tweets) - orig_length) + " tweets")
         return tweets
 
 
@@ -94,14 +103,15 @@ def baddadize(text):
         replaced_string = replaced_string.replace(term, "bad dad")
     return replaced_string
 
+
 def main():
     seen_tweets = set()
     tweets = [get_tweets_for_term(term, seen_tweets) +
               get_tweets_for_term(term + 's', seen_tweets) +
               get_tweets_for_term(term + "'s", seen_tweets) for term in terms]
     tweets = [item for sublist in tweets for item in sublist]
+    #print("\n\n".join([tweet['retweeted_status']['text'] for tweet in tweets]))
     my_latest_tweets = [tweet['text'] for tweet in twitter.get_user_timeline(count=5)]
-    print(my_latest_tweets)
     tweeted = False
     tries = 0
     while not(tweeted) and (tries < 10):
@@ -112,6 +122,7 @@ def main():
         if baddadized not in my_latest_tweets:
             twitter.update_status(status=baddadized)
             tweeted = True
+            twitter.create_friendship(screen_name=tweet['retweeted_status']['user']['screen_name'])
 
     if tweeted:
         print('tweeted: ' + baddadized + " after " + str(tries) + " tries")
